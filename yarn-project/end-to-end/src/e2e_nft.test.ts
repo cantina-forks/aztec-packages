c35f3a006dade06b2915cfe1dad3789f4e257594
benesjan <janbenes1234@gmail.com>
2024-08-30 14:14:25 +0000

    WIP

--- yarn-project/end-to-end/src/e2e_nft.test.ts
+++ yarn-project/end-to-end/src/e2e_nft.test.ts
@@ -16,7 +16,9 @@ describe('NFT', () => {
   let pxe: PXE;
   let teardown: () => Promise<void>;
 
-  let nftContract: NFTContract;
+  let nftContractAsAdmin: NFTContract;
+  let nftContractAsMinter: NFTContract;
+  let nftContractAsUser: NFTContract;
 
   let adminWallet: AccountWallet;
   let minterWallet: AccountWallet;
@@ -30,16 +32,25 @@ describe('NFT', () => {
     [adminWallet, minterWallet, userWallet] = wallets;
 
 
-    nftContract = await NFTContract.deploy(adminWallet, adminWallet.getAddress(), 'FROG', 'FRG').send().deployed();
+    nftContractAsAdmin = await NFTContract.deploy(adminWallet, adminWallet.getAddress(), 'FROG', 'FRG').send().deployed();
+    nftContractAsMinter = await NFTContract.at(nftContractAsAdmin.address, minterWallet);
+    nftContractAsUser = await NFTContract.at(nftContractAsAdmin.address, userWallet);
   });
 
   afterAll(() => teardown());
 
+  // NOTE: This test is sequential and each test case depends on the previous one
   it('sets minter', async () => {
-    await nftContract.methods.set_minter(minterWallet.getAddress(), true).send().wait();
+    await nftContractAsAdmin.methods.set_minter(minterWallet.getAddress(), true).send().wait();
   });
 
   it('minter mints to a user', async () => {
-    await nftContract.methods.mint(userWallet.getAddress(), TOKEN_ID).send().wait();
+    await nftContractAsMinter.methods.mint(userWallet.getAddress(), TOKEN_ID).send().wait();
+  });
+
+  it('user shields', async () => {
+    // In a simple shield flow the finalizer is the user itself (in the uniswap swap to shield flow it would be the uniswap contract)
+    const finalizer = userWallet.getAddress();
+    await nftContractAsUser.methods.prepare_shield(finalizer).send().wait();
   });
 });
