c1dad71e0a4dd84036fae67fdadcd70be7ece208
benesjan <janbenes1234@gmail.com>
2024-09-02 08:42:28 +0000

    WIP

--- yarn-project/end-to-end/src/e2e_nft.test.ts
+++ yarn-project/end-to-end/src/e2e_nft.test.ts
@@ -1,6 +1,14 @@
 import {
-  type AccountWallet, type AztecNode, type PXE
+  type AccountWallet,
+  AztecAddress,
+  type AztecNode,
+  BatchCall,
+  ExtendedNote,
+  Fr,
+  Note,
+  type PXE,
 } from '@aztec/aztec.js';
+import { deriveStorageSlotInMap } from '@aztec/circuits.js/hash';
 import { NFTContract } from '@aztec/noir-contracts.js';
 
 import { jest } from '@jest/globals';
@@ -18,23 +26,27 @@ describe('NFT', () => {
 
   let nftContractAsAdmin: NFTContract;
   let nftContractAsMinter: NFTContract;
-  let nftContractAsUser: NFTContract;
+  let nftContractAsUser1: NFTContract;
+  let nftContractAsUser2: NFTContract;
 
   let adminWallet: AccountWallet;
   let minterWallet: AccountWallet;
-  let userWallet: AccountWallet;
+  let user1Wallet: AccountWallet;
+  let user2Wallet: AccountWallet;
 
   const TOKEN_ID = 6;
 
   beforeAll(async () => {
     let wallets: AccountWallet[];
-    ({ aztecNode, pxe, teardown, wallets } = await setup(3));
-    [adminWallet, minterWallet, userWallet] = wallets;
+    ({ aztecNode, pxe, teardown, wallets } = await setup(4));
+    [adminWallet, minterWallet, user1Wallet, user2Wallet] = wallets;
 
-
-    nftContractAsAdmin = await NFTContract.deploy(adminWallet, adminWallet.getAddress(), 'FROG', 'FRG').send().deployed();
+    nftContractAsAdmin = await NFTContract.deploy(adminWallet, adminWallet.getAddress(), 'FROG', 'FRG')
+      .send()
+      .deployed();
     nftContractAsMinter = await NFTContract.at(nftContractAsAdmin.address, minterWallet);
-    nftContractAsUser = await NFTContract.at(nftContractAsAdmin.address, userWallet);
+    nftContractAsUser1 = await NFTContract.at(nftContractAsAdmin.address, user1Wallet);
+    nftContractAsUser2 = await NFTContract.at(nftContractAsAdmin.address, user2Wallet);
   });
 
   afterAll(() => teardown());
@@ -45,12 +57,55 @@ describe('NFT', () => {
   });
 
   it('minter mints to a user', async () => {
-    await nftContractAsMinter.methods.mint(userWallet.getAddress(), TOKEN_ID).send().wait();
+    await nftContractAsMinter.methods.mint(user1Wallet.getAddress(), TOKEN_ID).send().wait();
+
+    const ownerAfterMint = await nftContractAsUser1.methods.owner_of(TOKEN_ID).simulate();
+    expect(ownerAfterMint).toEqual(user1Wallet.getAddress());
   });
 
   it('user shields', async () => {
-    // In a simple shield flow the finalizer is the user itself (in the uniswap swap to shield flow it would be the uniswap contract)
-    const finalizer = userWallet.getAddress();
-    await nftContractAsUser.methods.prepare_shield(finalizer).send().wait();
+    // In a simple shield flow the finalizer is the user itself (in the uniswap swap to shield flow it would be
+    // the uniswap contract)
+    const finalizer = user1Wallet.getAddress();
+    const randomness = Fr.random();
+
+    const { txHash } = await new BatchCall(user1Wallet, [
+      nftContractAsUser1.methods.prepare_shield(finalizer, randomness).request(),
+      nftContractAsUser1.methods.send_to_shield(finalizer, TOKEN_ID, 0).request(),
+    ])
+      .send()
+      .wait();
+
+    const publicOwnerAfter = await nftContractAsUser1.methods.owner_of(TOKEN_ID).simulate();
+    expect(publicOwnerAfter).toEqual(AztecAddress.ZERO);
+
+    // TODO(#8238): Since we don't yet have a partial note delivery we have to manually add it to PXE
+    const nftNote = new Note([
+      new Fr(TOKEN_ID),
+      user1Wallet.getCompleteAddress().publicKeys.masterNullifierPublicKey.hash(),
+      randomness,
+    ]);
+
+    await user1Wallet.addNote(
+      new ExtendedNote(
+        nftNote,
+        user1Wallet.getAddress(),
+        nftContractAsUser1.address,
+        deriveStorageSlotInMap(NFTContract.storage.private_nfts.slot, user1Wallet.getAddress()),
+        NFTContract.notes.NFTNote.id,
+        txHash,
+      ),
+    );
+
+    // TODO: check "transient storage" was correctly reset
+    // const txEffect = await aztecNode.getTxEffect(txHash);
+    // console.log('txEffect', txEffect);
+  });
+
+  it('user privately sends', async () => {
+    await nftContractAsUser1.methods
+      .transfer_from(user1Wallet.getAddress(), user2Wallet.getAddress(), TOKEN_ID, 0)
+      .send()
+      .wait();
   });
 });
